c8379f4c5b47ead16cff3a9634143701809c129f
Niek van der Maas <mail@niekvandermaas.nl>
2020-06-17 12:31:35 +0000

    Extract CSS to separate file for speed

--- rollup.config.js
+++ rollup.config.js
@@ -19,16 +19,13 @@ export default {
 	},
 	plugins: [
 		svelte({
-			// enable run-time checks when not in production
 			dev: !production,
-			// we'll extract any component CSS out into
-			// a separate file - better for performance
-			/*css: css => {
+			css: css => {
 				css.write('public/build/bundle.css');
-			}*/
+			}
 		}),
 
-		postcss({plugins: (production ? [purgecss({content: ["./src/**/*.svelte", "./public/**/*.html"]})] : []), minimize: production}),
+		postcss({extract: true, plugins: (production ? [purgecss({content: ["./src/**/*.svelte", "./public/**/*.html"]})] : []), minimize: production}),
 
 		resolve({
 			browser: true,
