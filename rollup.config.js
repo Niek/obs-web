9295b7f863cd19f4a3297a446e7d913cafb9f93b
Niek van der Maas <mail@niekvandermaas.nl>
2020-06-23 12:13:37 +0200

    Generate index, use versioned bundle filenames

--- rollup.config.js
+++ rollup.config.js
@@ -6,26 +6,28 @@ import { terser } from 'rollup-plugin-terser';
 import postcss from 'rollup-plugin-postcss';
 import purgecss from '@fullhuman/postcss-purgecss';
 import nodePolyfills from 'rollup-plugin-node-polyfills';
+import html from '@rollup/plugin-html';
 
 const production = !process.env.ROLLUP_WATCH;
+const version = String(require('child_process').execSync('git rev-list --count HEAD')).trim(); // version number based on GIT commit
 
 export default {
 	input: 'src/main.js',
 	output: {
-		sourcemap: true,
+		sourcemap: !production,
 		format: 'iife',
 		name: 'app',
-		file: 'public/build/bundle.js'
+		file: 'public/bundle-v' + version + '.js'
 	},
 	plugins: [
 		svelte({
 			dev: !production,
 			css: css => {
-				css.write('public/build/bundle.css');
+				css.write('public/bundle-v' + version + '.css', !production);
 			}
 		}),
 
-		postcss({extract: true, plugins: (production ? [purgecss({content: ["./src/**/*.svelte", "./public/**/*.html"]})] : []), minimize: production}),
+		postcss({ extract: true, plugins: (production ? [purgecss({ content: ["./src/**/*.svelte", "./public/**/*.html"] })] : []), minimize: production }),
 
 		resolve({
 			browser: true,
@@ -33,7 +35,42 @@ export default {
 		}),
 		commonjs(),
 		nodePolyfills(),
-		
+
+		html({
+			template: async ({ attributes, files, meta, publicPath, title }) => {
+				const script = (files.js || [])
+					.map(({ fileName }) => {
+						return `<script defer src='${fileName}'></script>`;
+					})
+					.join('\n');
+
+				const css = (files.css || [])
+					.map(({ fileName }) => {
+						return `<link rel='stylesheet' href='${fileName}'>`;
+					})
+					.join('\n');
+				return `<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset='utf-8'>
+	<meta name='viewport' content='width=device-width,initial-scale=1'>
+	<meta http-equiv='origin-trial' content='ArcEc1taNHMu4hv4uJ0EqaaarH4y4amJM0PAuYQbWz8jQ7PKsDlfqI60XiQEtUGC6rPyIX0a/w9bErcnW28RDgsAAABReyJvcmlnaW4iOiJodHRwczovL29icy13ZWIubmllay50djo0NDMiLCJmZWF0dXJlIjoiV2FrZUxvY2siLCJleHBpcnkiOjE1OTQxNjYzOTl9'>
+	<meta name='apple-mobile-web-app-capable' content='yes'>
+
+	<title>OBS-web</title>
+
+	<link rel='icon' type='image/png' href='favicon.png'>
+	${css}
+	<link rel='manifest' href='manifest.json'>
+
+	${script}
+</head>
+
+<body>
+</body>
+</html>`;
+			}
+		}),
 		!production && serve(),
 		!production && livereload('public'),
 		production && terser()
