025ea8ed884707078b304e1b7e19ca2fe88543f6
Niek van der Maas <mail@niekvandermaas.nl>
2020-07-14 08:57:16 +0200

    Update deps, fix build error

--- src/App.svelte
+++ src/App.svelte
@@ -45,10 +45,9 @@
     if ($navbarBurgers.length > 0) {
       $navbarBurgers.forEach(el => {
         el.addEventListener('click', () => {
-          const target = el.dataset.target;
-          const $target = document.getElementById(target);
+          const target = document.getElementById(el.dataset.target);
           el.classList.toggle('is-active');
-          $target.classList.toggle('is-active');
+          target.classList.toggle('is-active');
         });
       });
     }
