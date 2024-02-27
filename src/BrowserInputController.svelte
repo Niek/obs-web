246a4cd4d3617533e3385997bb287bb8a045ed5d
Zorba Jobs Wong <c-wong@unext.jp>
2024-02-27 20:07:27 +0900

    Added browser input control

---
+++ src/BrowserInputController.svelte
@@ -0,0 +1,91 @@
+<script>
+  import { onMount } from 'svelte';
+  import { sendCommand } from './obs.js';
+
+  let showMenu = false;
+  let browserInputSetting = {};
+
+  onMount(async function () {
+    const data = await sendCommand('GetInputSettings', {
+      inputName: "Overlay"
+    });
+    browserInputSetting = data.inputSettings
+  });
+
+  function toggleShowMenu() {
+    showMenu = !showMenu;
+  }
+
+  async function setBrowserInputSetting() {
+    try {
+      await sendCommand('SetInputSettings', {
+        inputName: 'Overlay',
+        inputSettings:{
+          url: browserInputSetting.url,
+          height: browserInputSetting.height,
+          width: browserInputSetting.width
+        },
+        overlay: true
+      });
+      alert('Browser settings have been successfully updated.');
+    } catch (error) {
+      alert('ERROR: Update streaming server setting failed.');
+    }
+  }
+</script>
+
+<div class="dropdown mx-1 my-1" class:is-active={showMenu}>
+  <div class="dropdown-trigger">
+    <button
+      class="button is-info"
+      aria-haspopup="true"
+      aria-controls="browser-input-setting-dropdown-menu"
+      on:click={toggleShowMenu}
+    >
+      <span>Browser Input Settings</span>
+    </button>
+  </div>
+  <div class="dropdown-menu" id="browser-input-setting-dropdown-menu" role="menu">
+    <div class="dropdown-content">
+      <div class="dropdown-item">
+        <p class="mb-2">Overlay URL</p>
+        <input
+          class="input"
+          type="text"
+          placeholder="Server"
+          bind:value={browserInputSetting.url}
+        />
+      </div>
+      <hr class="dropdown-divider" />
+      <div class="dropdown-item">
+        <p class="mb-2">Height</p>
+        <input
+          class="input"
+          type="text"
+          placeholder="Height(in pixels)"
+          bind:value={browserInputSetting.height}
+        />
+      </div>
+      <hr class="dropdown-divider" />
+      <div class="dropdown-item">
+        <p class="mb-2">Width</p>
+        <input
+          class="input"
+          type="text"
+          placeholder="Width(in pixels)"
+          bind:value={browserInputSetting.width}
+        />
+      </div>
+      <div class="dropdown-item dropdown-item-right">
+        <button class="button is-danger" on:click={setBrowserInputSetting}>保存</button
+        >
+      </div>
+    </div>
+  </div>
+</div>
+
+<style>
+  .dropdown-item-right {
+    text-align: right;
+  }
+</style>
