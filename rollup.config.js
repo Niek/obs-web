3337ba9852cf729ce6a77a3471672c34cc25cb95
Niek van der Maas <mail@niekvandermaas.nl>
2024-11-04 13:15:42 +0100

    fix: use new mobile web app capability

--- rollup.config.js
+++ rollup.config.js
@@ -59,7 +59,7 @@ export default {
 <head>
   <meta charset='utf-8'>
   <meta name='viewport' content='width=device-width,initial-scale=1'>
-  <meta name='apple-mobile-web-app-capable' content='yes'>
+  <meta name='"mobile-web-app-capable' content='yes'>
 
   <title>OBS-web</title>
 
