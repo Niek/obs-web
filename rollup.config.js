f169a688281e62bf714e205b26df6e5acef3ac78
Niek van der Maas <mail@niekvandermaas.nl>
2025-09-18 14:03:52 +0200

    Bump to Bulma 1

--- rollup.config.js
+++ rollup.config.js
@@ -54,7 +54,7 @@ export default {
           })
           .join('\n')
         return `<!DOCTYPE html>
-<html lang="en">
+<html lang='en' data-theme='light'>
 <head>
   <meta charset='utf-8'>
   <meta name='viewport' content='width=device-width,initial-scale=1'>
